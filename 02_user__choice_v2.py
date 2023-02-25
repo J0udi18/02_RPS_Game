0bd3ed29886b2b7e3b8d0da4e4b98b1dade56df6
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-02-25 20:11:46 +1300

    Add files via upload

--- 02_user__choice_v2.py
+++ 02_user__choice_v2.py
@@ -1,4 +1,44 @@
 # Version 2 - error message included when calling function
 
 
+# Functions go here
+def choice_checker(question):
+
+       valid = False
+       while not valid:
+
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question) .lower()
+
+        if response == "r" or response == "rock":
+            return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
+        
+
+        elif response == "xxx":
+            return response
+        else: 
+            print(error) 
+
+# Main routine goes here 
+
+
+# Loop for testing purposes
+user_chice = ""
+while user_chice != "xxx":
+
+    # Ask user for choice and oheck it's valid
+    # user_choice = choice_checker("choose rock / paper / "
+                                   "scissors (r/p/s): ", 
+                                   "please choose from rock / " 
+                                   "peper / scissors "
+                                   "(or xxx to quit)"
+
+     # print out ohice for chomparsion purposes
+    print("you chose {}".format(user_choice))
+
+
 
